dc7d95cc4e6efdf4cd0f96083ad2024d072fa6a7
Vikash <vikashpathak041@gmail.com>
2022-05-26 17:53:31 +0530

    employee wage half day full day

--- src/com/bridglabz/employeeWage/EmployeeWage.java
+++ src/com/bridglabz/employeeWage/EmployeeWage.java
@@ -6,14 +6,22 @@ public class EmployeeWage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
-      
-		Random random= new Random();
-		int attendance=random.nextInt(2);
-		
-		if(attendance == 1)
+
+		Random random = new Random();
+		int attendance = random.nextInt(2);
+
+		int wagePerHour = 20;
+		int fullDayHours = 8;
+		int wagePerDay = 0;
+
+		if (attendance == 1) {
 			System.out.println("Employee Present..");
-		else
-			System.out.println("Employee Absent..");
-	 }
+			wagePerDay = wagePerHour * fullDayHours;
+		} else
+			System.out.println("Employee Absent");
+
+		System.out.println("Employee Wage Per Day: " + wagePerDay);
+
+	}
 
 }
