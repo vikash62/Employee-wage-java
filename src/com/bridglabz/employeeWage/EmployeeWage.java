5189cde290fd8f0598c5097a3dbef3dabd28442a
Vikash <vikashpathak041@gmail.com>
2022-05-26 17:48:21 +0530

    check Employee absent or present

--- src/com/bridglabz/employeeWage/EmployeeWage.java
+++ src/com/bridglabz/employeeWage/EmployeeWage.java
@@ -1,7 +1,19 @@
 package com.bridglabz.employeeWage;
 
+import java.util.Random;
+
 public class EmployeeWage {
-public static void main(String[] args) {
-	System.out.println("Employee wage");
-}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Program");
+      
+		Random random= new Random();
+		int attendance=random.nextInt(2);
+		
+		if(attendance == 1)
+			System.out.println("Employee Present..");
+		else
+			System.out.println("Employee Absent..");
+	 }
+
 }
