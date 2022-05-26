9d43e296d7d85c873d8a19ff9e1cc6e0db201171
Vikash <vikashpathak041@gmail.com>
2022-05-26 18:49:06 +0530

    solving using switch case

--- src/com/bridglabz/employeeWage/EmployeeWage.java
+++ src/com/bridglabz/employeeWage/EmployeeWage.java
@@ -1,33 +1,36 @@
 package com.bridglabz.employeeWage;
 import java.util.Random;
 
+import java.util.Random;
+
 public class EmployeeWage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
 
 		Random random = new Random();
-		int attendance = random.nextInt(3);
+		int attendance = random.nextInt(3); // Generate 3 random number 0,1,2
 
 		int wagePerHour = 20;
-		int fullDayHours = 8;
-		int partTimeHours = 4;
+		int fullDayHour = 8;
+		int partTimeHour = 4;
 		int wagePerDay = 0;
 
 		switch (attendance) {
+
 		case 0:
-			System.out.println("Employee Absent.");
+			System.out.println("Employee Absent");
 			break;
+
 		case 1:
-			wagePerDay = partTimeHours * wagePerHour;
-			break;
+			System.out.println("Employee Part Time Present");
+			wagePerDay = wagePerHour * partTimeHour;
 		default:
-			wagePerDay = fullDayHours * wagePerHour;
-
+			System.out.println("Employee Full Day Present..");
+			wagePerDay = wagePerHour * fullDayHour;
 		}
- 
-		System.out.println("Employee Wage Per Day: " + wagePerDay);
 
+		System.out.println("Employee Wage Per Day :-" + wagePerDay);
 	}
 
 }
