636e606fe1a57513fa86e91f4fd8b36f8cd44cec
Vikash <vikashpathak041@gmail.com>
2022-06-04 22:22:41 +0530

    RefactorTheCodeByOOPSConcept

--- src/com/bridglabz/employeeWage/EmployeeWage.java
+++ src/com/bridglabz/employeeWage/EmployeeWage.java
@@ -1,43 +1,45 @@
 package com.bridglabz.employeeWage;
-import java.util.Random;
+//Employee Wage Computation Problems Using OOPS Concept
+//Use Class Method and Class Variables
+public class EmployeeWage
+{
+  public static final int IS_PART_TIME = 1;
+  public static final int IS_FULL_TIME = 2;
+  public static final int EMP_RATE_PER_HOUR = 20;
+  public static final int NUM_OF_WORKING_DAYS = 2;
+  public static final int MAX_HRS_IN_MONTH = 10;
 
-public class EmployeeWage {
-
-	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Program");
-
-		Random random = new Random();
-
-		int wagePerHour = 20;
-		int fullDayHour = 8;
-		int partTimeHour = 4;
-		int wagePerDay = 0;
-		int monthlySalary = 0;
-
-		for (int day = 1; day <= 20; day++) {
-
-			int attendance = random.nextInt(3); // Generate 3 random number 0,1,2
-			System.out.println("randomNumber"+attendance);
-			wagePerDay =0;
-			switch (attendance) {
-
-			case 0:
-				System.out.println("Employee Absent");
-				break;
-
-			case 1:
-				System.out.println("Employee Part Time Present");
-				wagePerDay = partTimeHour * wagePerHour;
-				break;
-			default:
-				System.out.println("Employee Full Day Present..");
-				wagePerDay = wagePerHour * fullDayHour;
-				break;
-			}
-
-			monthlySalary = monthlySalary + wagePerDay;
-			System.out.println(day + " : " + monthlySalary);
-		}
-	}
-
+  static void ComputeEmployeeWage()
+  {
+      //Variables
+      int empHrs = 0;
+      int totalEmpHrs = 0;
+      int totalWorkingDays = 0;
+      //Computation
+      while (totalEmpHrs <= MAX_HRS_IN_MONTH &&
+              totalWorkingDays < NUM_OF_WORKING_DAYS)
+      {
+          totalWorkingDays++;
+          int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+          switch (empCheck)
+          {
+              case IS_PART_TIME:
+                  empHrs = 4;
+                  break;
+              case IS_FULL_TIME:
+                  empHrs = 8;
+                  break;
+              default:
+                  empHrs = 0;
+          }
+          totalEmpHrs += empHrs;
+          System.out.println("Day: " + totalWorkingDays + " Emp Hr: " + empHrs);
+      }
+      int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+      System.out.println("Total Emp Wage: " + totalEmpWage);
+  }
+  public static void main(String args[])
+  {
+      ComputeEmployeeWage();
+  }
 }
